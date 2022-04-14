3a78865799dc68fb497f4f5766ec7ecfd2b6e9cf
Robinson Idagiza <robinadoro@gmail.com>
2022-04-14 03:19:38 +0300

    commit tree 08f7453c822f17b7ed909ede98e1b92cbe99fc1d

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let multiply, num1=1, num2=62;
+multiply=num1*num2;
+
+let random;
+random=Math.floor( Math.random()*10)
+
+let mod, num3=14, num4=5;
+mod= num3%num4;
+
+let max;
+max=Math.max(10, 1, 20, 9);
